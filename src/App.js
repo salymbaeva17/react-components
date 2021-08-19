67dcff9e42c578c4b956e673714f7bc658919a9b
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-19 21:32:13 +0600

    Initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Todos from "./Todos";
+
+const App = () => {
+    return (
+        <div>
+            {/*<LoginForm />*/}
+            {/*<Contacts />*/}
+            {/*<Figure />*/}
+            {/*<Counter />*/}
+            <Todos />
+        </div>
+    );
+};
+
+export default App;
