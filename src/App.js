863309f36e70fe62963eeab9fe6cb122475b0180
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-19 21:35:24 +0600

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Todos from "./Todos";
+import LoginForm from "./LoginForm";
+import Contacts from "./Contacts";
+import Figure from "./Figure";
+import Counter from "./Counter";
 
 const App = () => {
     return (
         <div>
-            {/*<LoginForm />*/}
-            {/*<Contacts />*/}
-            {/*<Figure />*/}
-            {/*<Counter />*/}
+            <LoginForm />
+            <Contacts />
+            <Figure />
+            <Counter />
             <Todos />
         </div>
     );
